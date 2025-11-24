a2d7e2a35e0f67fcf775ed8e5898a2ffa07ea040
MicaPersonal <miqa.orellano@gmail.com>
2025-11-24 04:11:13 -0300

    Fix video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
         className="absolute inset-0 w-full h-full object-cover brightness-75"
       >
         <source
-          src="public\home.mp4"
+          src="/home.mp4"
           type="video/mp4"
         />
       </video>
